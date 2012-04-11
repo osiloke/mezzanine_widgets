d97c67130a8cb8b76a1d124ab3aee451b3528642
Osiloke Emoekpere <osi@progwebtech.com>
2012-04-10 22:57:02 -0400

    Fixed setup.py to include template files

--- setup.py
+++ setup.py
@@ -4,6 +4,47 @@ import os
 import sys
 from setuptools import setup, find_packages 
 
+def fullsplit(path, result=None):
+    """
+Split a pathname into components (the opposite of os.path.join) in a
+platform-neutral way.
+"""
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == "":
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+
+package_dir = "widget"
+
+
+packages = []
+
+for dirpath, dirnames, filenames in os.walk(package_dir):
+    # ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith("."):
+            del dirnames[i]
+    if "__init__.py" in filenames:
+        packages.append(".".join(fullsplit(dirpath)))
+
+template_patterns = [
+    'templates/*.html',
+    'templates/*/*.html',
+    'templates/*/*/*.html',
+]
+
+package_data = dict(
+    (package_name, template_patterns)
+    for package_name in packages
+)
+
+
 
 CLASSIFIERS = [
     'Development Status :: 3 - Alpha',
@@ -28,7 +69,8 @@ setup(
     long_description=open('README.rst').read(),
     zip_safe=False,
     include_package_data=True,
-    packages=find_packages(exclude=('example',)),
+    package_data=package_data,
+    packages=packages,
     install_requires=[
                 "django >= 1.3",
                 "mezzanine",
