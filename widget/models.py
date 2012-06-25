eefe9a1e0dbf79186832a450b8293287063d540f
Lorin Hochstein <lorin@nimbisservices.com>
2012-06-25 10:15:36 -0400

    Plural of widget is widgets

--- widget/models.py
+++ widget/models.py
@@ -148,7 +148,7 @@ class Widget(Orderable, Ownable):
 
     class Meta:
         verbose_name = _("Widget")
-        verbose_name_plural = _("Widget's")
+        verbose_name_plural = _("Widgets")
         ordering = ("display_title",) 
 
 
